2c1aaaa0d10069033e39fa560df4d0f4a0ff39fc
Walter Gauto <waltergautofcb@gmail.com>
2023-09-08 16:26:07 -0400

    Add provider-variables.tf

---
+++ provider-variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key"
+  default = "AKIAXZXCQARTGHD57WJD"
+}
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key"
+  default = "nSgLd24GQs8ty64KloxbJQo7Asd8OMc0cvnoSFU8"
+}
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default = "us-east-1"
+}
