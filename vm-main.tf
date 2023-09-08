45792eea068a2d75579202e00ddf42666059e148
Walter Gauto <waltergautofcb@gmail.com>
2023-09-08 16:28:26 -0400

    Add vm-main.tf

---
+++ vm-main.tf
@@ -0,0 +1,68 @@
+# Define the security group for the EC2 Instance
+resource "aws_security_group" "aws-vm-sg" {
+  name        = "vm-sg"
+  description = "Allow incoming connections"
+  vpc_id      = aws_vpc.vpc.id  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTP connections"
+  }
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming RDP connections (Windows)"
+  }  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH connections (Linux)"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  tags = {
+    Name = "linux-sg"
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "vm-server" {
+  ami                    = data.aws_ami.ubuntu-linux-2004.id
+  instance_type          = var.vm_instance_type
+  subnet_id              = aws_subnet.public-subnet.id
+  vpc_security_group_ids = [aws_security_group.aws-vm-sg.id]
+  source_dest_check      = false
+  key_name               = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = var.vm_associate_public_ip_address
+
+
+  # root disk
+  root_block_device {
+    volume_size           = var.vm_root_volume_size
+    volume_type           = var.vm_root_volume_type
+    delete_on_termination = true
+    encrypted             = true
+  }
+  # extra disk
+  ebs_block_device {
+    device_name           = "/dev/xvda"
+    volume_size           = var.vm_data_volume_size
+    volume_type           = var.vm_data_volume_type
+    encrypted             = true
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "linux-server-vm"
+  }
+}
