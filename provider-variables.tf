26bce2a091355ef1dafd954eae3602fa8013d7b0
waltergauto <61380032+waltergauto@users.noreply.github.com>
2023-09-15 17:03:11 -0400

    Update provider-variables.tf
    Modify provider-variables

--- provider-variables.tf
+++ provider-variables.tf
@@ -1,14 +1,10 @@
 variable "aws_access_key" {
   type        = string
   description = "AWS access key"
-  #default = "AKIAXZXCQARTGHD57WJD"
-  default = "AKIAXZXCQARTMZVWAWUM"
 }
 variable "aws_secret_key" {
   type        = string
   description = "AWS secret key"
-  #default = "nSgLd24GQs8ty64KloxbJQo7Asd8OMc0cvnoSFU8"
-  default = "ZKAhHAbv0SsOCyX/4EuhyEcXCuKUXn62seAtju5k"
 }
 variable "aws_region" {
   type        = string
